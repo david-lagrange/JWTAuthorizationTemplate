f4b6c410fd63be0a93d6be1bf48fb880e4071138
David <davidnlagrange@hotmail.com>
2019-12-28 19:18:01 -0600

    Intialization of Project on GitHub

---
+++ JWTAuthenticationAshProgHelp/Controllers/LoginController.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using JWTAuthenticationAshProgHelp.Model;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+
+namespace JWTAuthenticationAshProgHelp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LoginController : ControllerBase
+    {
+        private IConfiguration _config;
+
+        public LoginController(IConfiguration config)
+        {
+            _config = config;
+        }
+
+        [HttpPost]
+        public IActionResult Login([FromBody] UserModel login)
+        {
+            IActionResult response = Unauthorized();
+
+            var user = AuthenticateUser(login);
+
+            if (user != null)
+            {
+                var tokenStr = GenerateJSONWebToken(user);
+                response = Ok(new { token = tokenStr });
+            }
+
+            return response;
+        }
+
+        
+
+        private UserModel AuthenticateUser(UserModel login)
+        {
+            // Static authentication for demo. Verify user in DB here
+            UserModel user = null;
+            if (login.UserName == "yourusername" && login.Password == "somepassword")
+            {
+                user = new UserModel { UserName = "yourusername", Email = "randomemail@devcodecamp.com", Password = "somepassword" };
+            }
+            return user;
+        }
+
+        private string GenerateJSONWebToken(UserModel user)
+        {
+            var securityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_config["Jwt:Key"]));
+            // Setting our application API secret encrypted key
+            var credentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256);
+
+            var claims = new[]
+            {
+                new Claim(JwtRegisteredClaimNames.Sub, user.UserName),
+                new Claim(JwtRegisteredClaimNames.Email, user.Email),
+                new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString())
+
+            };
+
+            var token = new JwtSecurityToken(
+                // Setting the issuer so we can check that the bearer token we recieve back was issued by us
+                issuer: _config["Jwt:Issuer"],
+                audience: _config["Jwt:Issuer"],
+                claims,
+                expires: DateTime.Now.AddMinutes(120),
+                // Adding this to confirm that the bearer token we recieve on request has the same encrypted key
+                signingCredentials: credentials);
+            var encodeToken = new JwtSecurityTokenHandler().WriteToken(token);
+            return encodeToken;
+        }
+
+        // Example methods that implement the Authorize attribute.
+
+        [Authorize]
+        [HttpPost("Post")]
+        public string Post()
+        {
+            var identity = HttpContext.User.Identity as ClaimsIdentity;
+            IList<Claim> claim = identity.Claims.ToList();
+            var userName = claim[0].Value;
+            return "Welcome: " + userName;
+        }
+
+        [Authorize]
+        [HttpGet("GetValue")]
+        public ActionResult<IEnumerable<string>> Get()
+        {
+            return new string[] { "Value1", "Value2", "Value3" };
+        }
+    }
+}
---
+++ JWTAuthenticationAshProgHelp/Model/UserModel.cs
@@ -0,0 +1,10 @@
+﻿namespace JWTAuthenticationAshProgHelp.Model
+{
+    public class UserModel
+    {
+        public string UserName { get; set; }
+        public string Password { get; set; }
+        public string Email { get; set; }
+
+    }
+}
